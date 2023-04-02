4747c33f1d0825b3aea35570c41d2418021daf5c
JoaoPDeveloper <joao_entreprise@hotmail.com>
2023-04-01 23:27:01 -0300

    conversor de dolar utilizando API
    conversor consumindo API do banco do brasil, valores atualizados a cada alteração no dolar!

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="stylesheet" href="style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Conversor de Dólar</title>
+</head>
+<body>
+  <header>
+    <h1>Conversor de Dólar</h1>
+  </header>
+  <main>
+    <?php 
+    //cotacao vinda da api do banco central
+        $inicio = date("m-d-Y" ,strtotime("-7 days"));
+        $fim = date("m-d-Y");
+        $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''. $inicio.'\'&@dataFinalCotacao=\''. $fim .'\'&27&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+        $dados = json_decode(file_get_contents($url), true);
+        
+        $cotacao =  $dados["value"][0]["cotacaoCompra"];
+        $real = $_REQUEST["din"] ?? 0;
+        $dolar = $real / $cotacao ;
+      
+     //formatação de moedas com internacionalização!
+     $padrao = numfmt_create("pt-BR", NumberFormatter::CURRENCY); 
+
+     echo "Seus " . numfmt_format_currency($padrao,$real,"BRL") . " equivalem a  <strong>" . numfmt_format_currency($padrao, $dolar, "USD") . "</strong>";
+
+    ?>
+    <br>
+    <br>
+          <button onclick="javascript:history.go(-1)">🔃 Voltar</button>
+  </main>
+</body>
+</html>
